b4a52a5d45b022fc9ddac8feae30ee009546f8eb
Praise-xo <praise.xx0@gmail.com>
2024-05-28 15:38:46 +0100

    main

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('IFEOLUWA PRAISE-GOD  VUG/CSC/21/5970'));
+app.get('/', (req, res) => res.send('Name: IFEOLUWA PRAISE-GOD, Matric: VUG/CSC/21/5970'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
